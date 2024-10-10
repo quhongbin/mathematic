074ce64e476622329ca8cdb57e3f60db61ebd4b0
hongbin <2818777520@qq.com>
2024-10-10 17:50:53 +0800

    增加注释

--- CProjection/point.c
+++ CProjection/point.c
@@ -15,10 +15,10 @@ Node* initLinkList(){
 
 void headInsert(Node* head_node,int data){
     Node* node=(Node*)malloc(sizeof(Node));
-    node->next=head_node->next;
-    node->data=data;
-    head_node->next=node;  //
-    head_node->data++;
+    node->next=head_node->next;             //将头节点的Next*赋给新建Node->next
+    node->data=data;                        //
+    head_node->next=node;                   //把头节点的Next*指向当前的新建的节点
+    head_node->data++;                      //
 };
 
 void tailInsert(Node* head_node,int data){
@@ -27,20 +27,20 @@ void tailInsert(Node* head_node,int data){
     node->data=data;
     node->next=NULL;
     head_node=head_node->next;
-    while (head_node->next)
-    {
-        head_node=head_node->next;
-    }
-    head_node->next=node;
+    while (head_node->next)                 //将Next指向NULL的指针指向这个代码段新建的节点Node
+    {                                       //然后把该新建的节点Next指针指向NULL
+        head_node=head_node->next;          //Node是按指针指向排序的(猜测是堆栈)
+    }                                       //
+    head_node->next=node;                   //可以假象头节点(head_node)始终在整个单链表的头部(即单链表的最左侧)
     head->data++;
 }
 
 void Delete(Node* head_node,int data){
-    Node* current = head_node->next;
-    Node* pre_node= head_node; 
-    while (current){
-        if (current->data==data){
-           pre_node->next=current->next; 
+    Node* current = head_node->next;        //设头节点为n,则当前节点current为n-1
+    Node* pre_node= head_node;              //
+    while (current){                        //
+        if (current->data==data){           //若当前数据是要删除的数据时,就将当前数据的Node的Next*赋给前一个Node的Next*
+           pre_node->next=current->next;    //即当Node n-1 是要被删除的,那么将Node n->next指向Node n-2
            free(current);
            break;
         }
@@ -53,7 +53,7 @@ void Delete(Node* head_node,int data){
 
 
 void printList(Node* input_list){
-    input_list=input_list->next;
+    input_list=input_list->next;            //将第一个Node拿出来,因为头节点不打印
     while (input_list)
     {
         printf("%d",input_list->data);
